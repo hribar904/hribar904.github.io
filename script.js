81e7b275f844fb1f6154f4461d9d1578d1b99d68
abe hribar <abe.hribar@gmail.com>
2023-04-17 14:57:14 -0400

    Updating portfolio website 17APR2023

---
+++ script.js
@@ -0,0 +1,7 @@
+// Phone Number Link
+const phoneNumber = document.querySelector('.phone-number');
+phoneNumber.href = 'tel:4405378594';
+
+// Email Link
+const email = document.querySelector('.email');
+email.href = 'mailto:abe.hribar@gmail.com';
